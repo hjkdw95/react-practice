b8909cc2c2479310a1314328d2901f8064b64571
Casey <hjkdw9506@gmail.com>
2021-05-18 15:27:06 +0900

    🎨  add input form

---
+++ src/components/habitAddForm.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+
+class HabitAddForm extends Component {
+  // react DOM요소 input 값에 접근하는 방법 - input에 ref로 연결해야됨
+  formRef = React.createRef()
+  inputRef = React.createRef()
+  onSubmit = (event) => {
+    //onSubmit의 디폴트 상태인 입력 후 refresh를 방지하게 해준다
+    event.preventDefault()
+    const name = this.inputRef.current.value
+    name && this.props.onAdd(name)
+    //this.inputRef.current.value = ''
+    this.formRef.current.reset()
+  }
+  render() {
+    return (
+      <form ref={this.formRef} className="add-form" onSubmit={this.onSubmit}>
+        <input
+          ref={this.inputRef}
+          type="itext"
+          className="add-input"
+          placeholder="Please enter your habit"
+        ></input>
+        <button className="add-button">Add</button>
+      </form>
+    )
+  }
+}
+
+export default HabitAddForm
--- src/components/habits.jsx
+++ src/components/habits.jsx
@@ -1,21 +1,25 @@
 import React, { Component } from 'react'
 import Habit from './habit'
+import HabitAddform from './habitAddForm'
 
 // 여러가지 습관을 표시한다
 class Habits extends Component {
   render() {
     return (
-      <ul>
-        {this.props.habits.map((habit) => (
-          <Habit
-            key={habit.id}
-            habit={habit}
-            onIncrement={this.props.onIncrement}
-            onDecrement={this.props.onDecrement}
-            onDelete={this.props.onDelete}
-          />
-        ))}
-      </ul>
+      <>
+        <HabitAddform onAdd={this.props.onAdd} />
+        <ul>
+          {this.props.habits.map((habit) => (
+            <Habit
+              key={habit.id}
+              habit={habit}
+              onIncrement={this.props.onIncrement}
+              onDecrement={this.props.onDecrement}
+              onDelete={this.props.onDelete}
+            />
+          ))}
+        </ul>
+      </>
     )
   }
 }
